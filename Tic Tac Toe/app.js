21158b2d06c480b9f6476f56c1bde66bce73f0ce
Casey Lakritz <caseylakritz@Caseys-MacBook-Pro.local>
2015-09-26 19:45:25 -0400

    Display winner and fixed player click bug

--- Tic Tac Toe/app.js
+++ Tic Tac Toe/app.js
@@ -18,7 +18,6 @@
 
 console.log("WORKING");
 
-var player = 1;
 
 var s1 = $('#s1'),
 	s2 = $('#s2'),
@@ -29,56 +28,110 @@ var s1 = $('#s1'),
 	s7 = $('#s7'),
 	s8 = $('#s8'),
 	s9 = $('#s9'),
-	reset = $('#reset');
+	reset = $('#reset'),
+	playerX = $('<div class="player-score"></div>'),
+	playerO = $('<div class="player-score"></div>');
 
-s1.click(playerMove);
-s2.click(playerMove);
-s3.click(playerMove);
-s4.click(playerMove);
-s5.click(playerMove);
-s6.click(playerMove);
-s7.click(playerMove);
-s8.click(playerMove);
-s9.click(playerMove);
+startGame();
+
+function startGame () {
+	player = 1;
+	s1.click(playerMove);
+	s2.click(playerMove);
+	s3.click(playerMove);
+	s4.click(playerMove);
+	s5.click(playerMove);
+	s6.click(playerMove);
+	s7.click(playerMove);
+	s8.click(playerMove);
+	s9.click(playerMove);
+	reset.click(resetGame);
+}
+
 
 
 function playerMove (event) {
+	var move;
 	var btn = $(this);
-	if (player == 1) {
-		btn.val('X');
-		btn.off();
-		determineWinner();
-		player += 1;
+	if (player % 2 != 0) {
+		move = 'X';
 	} else {
-		btn.val('O');
-		btn.off();
-		determineWinner();
-		player -= 1;
-	};
+		move = 'O';
+	}
+	btn.val(move);
+	btn.off();
+
+	if (determineWinner()) {
+		// gameOver();
+	} else {
+		player += 1;
+	}
 };
 
 function determineWinner () {
 	var h2 = $('#show-winner');
-	if (s1.val() == s2.val() && s2.val() == s3.val()) {
-		h2.text("WIINER! Player " + s1.val() + " has won!");
-	} else if (s4.val() == s5.val() && s5.val() == s6.val()) {
-		h2.text("WIINER! Player " + s4.val() + " has won!");
-	} else if (s7.val() == s8.val() && s8.val() == s9.val()) {
-		h2.text("WIINER! Player " + s7.val() + " has won!");
-	} else if (s1.val() == s4.val() && s4.val() == s7.val()) {
-		h2.text("WIINER! Player " + s1.val() + " has won!");
-	} else if (s2.val() == s5.val() && s5.val() == s8.val()) {
-		h2.text("WIINER! Player " + s2.val() + " has won!");
-	} else if (s3.val() == s6.val() && s6.val() == s9.val()) {
+	if (s1.val() == s2.val() && s2.val() == s3.val() && s1.val() != "") {
+		h2.text("WINNER! Player " + s1.val() + " has won!");
+	} else if (s4.val() == s5.val() && s5.val() == s6.val() && s4.val() != "") {
+		h2.text("WINNER! Player " + s4.val() + " has won!");
+	} else if (s7.val() == s8.val() && s8.val() == s9.val() && s7.val() != "") {
+		h2.text("WINNER! Player " + s7.val() + " has won!");
+	} else if (s1.val() == s4.val() && s4.val() == s7.val() && s1.val() != "") {
+		h2.text("WINNER! Player " + s1.val() + " has won!");
+	} else if (s2.val() == s5.val() && s5.val() == s8.val() && s2.val() != "") {
+		h2.text("WINNER! Player " + s2.val() + " has won!");
+	} else if (s3.val() == s6.val() && s6.val() == s9.val() && s3.val() != "") {
 		h2.text("WIINER! Player " + s3.val() + " has won!");
-	} else if (s1.val() == s5.val() && s5.val() == s9.val()) {
-		h2.text("WIINER! Player " + s1.val() + " has won!");
-	} else if (s3.val() == s5.val() && s5.val() == s7.val()) {
-		h2.text("WIINER! Player " + s3.val() + " has won!");
-	} else {
-		player = player;
+	} else if (s1.val() == s5.val() && s5.val() == s9.val() && s1.val() != "") {
+		h2.text("WINNER! Player " + s1.val() + " has won!");
+	} else if (s3.val() == s5.val() && s5.val() == s7.val() && s3.val() != "") {
+		h2.text("WINNER! Player " + s3.val() + " has won!");
 	}
+};
+
+function gameOver() {
+	s1.off();
+	s2.off();
+	s3.off();
+	s4.off();
+	s5.off();
+	s6.off();
+	s7.off();
+	s8.off();
+	s9.off();
+	reset.off();
 }
+
+function resetGame () {
+	player = 1;
+	s1.off();
+	s2.off();
+	s3.off();
+	s4.off();
+	s5.off();
+	s6.off();
+	s7.off();
+	s8.off();
+	s9.off();
+	reset.off();
+
+	s1.val("");
+	s2.val("");
+	s3.val("");
+	s4.val("");
+	s5.val("");
+	s6.val("");
+	s7.val("");
+	s8.val("");
+	s9.val("");
+	startGame();
+}
+
+
+
+
+
+
 
 
 
